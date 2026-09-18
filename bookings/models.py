f60352e9716cc4498ddf9112b6eaa24eade240d2
calebezak@gmail.com <calebezak@gmail.com>
2026-09-18 22:50:42 +0100

    Implementing Review class

--- bookings/models.py
+++ bookings/models.py
@@ -35,3 +35,13 @@ class Assignee(models.Model):
 
     def __str__(self):
         return f'Assignment #{self.pk} - {self.provider} ({self.get_assignmentstatus_display()})'
+
+
+class Review(models.Model):
+    booking = models.OneToOneField(Booking, on_delete=models.CASCADE, related_name='review')
+    rating = models.PositiveSmallIntegerField()
+    comment = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Review for Booking #{self.booking.pk} - {self.rating}/5'
