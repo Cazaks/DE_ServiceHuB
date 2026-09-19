f996546bbdca6ca92746f89d09e12a30a94074a5
calebezak@gmail.com <calebezak@gmail.com>
2026-09-19 05:10:41 +0100

    Creating account views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Customer, Provider, ProviderAgreement
+from .serializers import CustomerSerializer, ProviderSerializer, ProviderAgreementSerializer
 
-# Create your views here.
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+class ProviderViewSet(viewsets.ModelViewSet):
+    queryset = Provider.objects.all()
+    serializer_class = ProviderSerializer
+
+
+class ProviderAgreementViewSet(viewsets.ModelViewSet):
+    queryset = ProviderAgreement.objects.all()
+    serializer_class = ProviderAgreementSerializer
