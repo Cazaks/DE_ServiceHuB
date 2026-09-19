05c9f758ed21f803369ea10d1fa0a61b0a5d2a21
calebezak@gmail.com <calebezak@gmail.com>
2026-09-19 05:59:31 +0100

    Implementing payment views

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import CustomerPayment, ProviderPayout
+from .serializers import CustomerPaymentSerializer, ProviderPayoutSerializer
 
-# Create your views here.
+
+class CustomerPaymentViewSet(viewsets.ModelViewSet):
+    queryset = CustomerPayment.objects.all()
+    serializer_class = CustomerPaymentSerializer
+
+
+class ProviderPayoutViewSet(viewsets.ModelViewSet):
+    queryset = ProviderPayout.objects.all()
+    serializer_class = ProviderPayoutSerializer
