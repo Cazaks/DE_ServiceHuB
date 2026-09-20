09713d5f934f78869d219fa13fc4530be2d757a6
calebezak@gmail.com <calebezak@gmail.com>
2026-09-20 06:07:34 +0100

    Adding APITestCase permission tests for CustomerViewSet and ProviderViewSet

--- accounts/tests.py
+++ accounts/tests.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from .models import Customer, Provider
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
 
 
 class CustomerModelTests(TestCase):
@@ -52,5 +54,69 @@ class PasswordHashingTests(TestCase):
     def test_check_password_rejects_wrong_password(self):
         user = User.objects.create_user(username='hashcheck4', password='testpass123')
         self.assertFalse(user.check_password('wrongpassword'))
+
+
+class CustomerViewSetPermissionTests(APITestCase):
+    def setUp(self):
+        admin = User.objects.create_superuser(username='admin1', password='pass12345')
+        self.admin_token = Token.objects.create(user=admin)
+
+        cust_user = User.objects.create_user(username='cust1', password='pass12345')
+        self.customer = Customer.objects.create(user=cust_user, phone_number='08011112222')
+        self.customer_token = Token.objects.create(user=cust_user)
+
+        prov_user = User.objects.create_user(username='prov1', password='pass12345')
+        Provider.objects.create(user=prov_user, phone_number='08033334444')
+        self.provider_token = Token.objects.create(user=prov_user)
+
+    def test_admin_sees_all_customers(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.admin_token.key}')
+        response = self.client.get('/api/accounts/customers/')
+        self.assertEqual(len(response.data), 1)
+
+    def test_provider_sees_no_customers(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.provider_token.key}')
+        response = self.client.get('/api/accounts/customers/')
+        self.assertEqual(len(response.data), 0)
+
+    def test_customer_sees_only_own_record(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.customer_token.key}')
+        response = self.client.get('/api/accounts/customers/')
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['id'], self.customer.id)
+
+    def test_anonymous_request_is_rejected(self):
+        response = self.client.get('/api/accounts/customers/')
+        self.assertEqual(response.status_code, 401)
+
+
+class ProviderViewSetPermissionTests(APITestCase):
+    def setUp(self):
+        admin = User.objects.create_superuser(username='admin2', password='pass12345')
+        self.admin_token = Token.objects.create(user=admin)
+
+        cust_user = User.objects.create_user(username='cust2', password='pass12345')
+        Customer.objects.create(user=cust_user, phone_number='08011112223')
+        self.customer_token = Token.objects.create(user=cust_user)
+
+        prov_user = User.objects.create_user(username='prov2', password='pass12345')
+        self.provider = Provider.objects.create(user=prov_user, phone_number='08033334445')
+        self.provider_token = Token.objects.create(user=prov_user)
+
+    def test_admin_sees_all_providers(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.admin_token.key}')
+        response = self.client.get('/api/accounts/providers/')
+        self.assertEqual(len(response.data), 1)
+
+    def test_customer_sees_no_providers(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.customer_token.key}')
+        response = self.client.get('/api/accounts/providers/')
+        self.assertEqual(len(response.data), 0)
+
+    def test_provider_sees_only_own_record(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.provider_token.key}')
+        response = self.client.get('/api/accounts/providers/')
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['id'], self.provider.id)
 
 
